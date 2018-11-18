ce15efb18859bbdec83cd6aab1ce76968ce146f3
arvera <arvera@ca.ibm.com>
2018-11-18 07:23:57 -0500

    adding a easy shell script to create new posts

---
+++ _posts/newPost.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "ERROR: No arguments supplied"
+    echo "       The post requires a title, with no spaces. ex: something_something_more"
+    exit -1
+fi
+
+POST_FILENAME=$(date +'%Y-%m-%d')-$1
+touch $POST_FILENAME
+
+echo "A new post created with the name of"
+echo "     $PWD/$POST_FILENAME"
